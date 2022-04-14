c105de407006d33ccd4a9f5c17b6845d54a26421
Rasool Poursangari <raspsgri@gmail.com>
2022-04-14 04:02:23 +0200

    stationOne

--- class_project.py
+++ class_project.py
@@ -1,15 +1,26 @@
 class Project:
+    progress = 1
+
     def __init__(self, index, name):
         self.index = index
         self.name = name
+        self.availability = 0
         self.webpage = 'https://raspsgri.com/' + name + '/'
 
     def full_info(self):
-        return '{} {} {}'.format(self.index, self.name, self.webpage)
+        return '{} {} {} {}'.format(self.index, self.name, self.availability, self.webpage)
+
+    def show_availability(self):
+        self.availability = int(self.availability * self.progress)
 
 
 project_1 = Project(0, 'tiwa')
 project_2 = Project(1, 'fdsi')
+
+project_1.progress = 43
+project_2.progress = 23
+project_1.show_availability()
+project_2.show_availability()
 
 print(project_1.full_info())
 print(project_2.full_info())
