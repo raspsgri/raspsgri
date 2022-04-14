c54641f367921026d2c582b72189ad217d66b5ed
Rasool Poursangari <raspsgri@gmail.com>
2022-04-14 03:06:50 +0200

    stationOne

--- class_project.py
+++ class_project.py
@@ -1,8 +1,12 @@
 class Project:
-    def __init__(self, name, index):
-        self.name = name
+    def __init__(self, index, name):
         self.index = index
+        self.name = name
         self.webpage = 'https://raspsgri.com/' + name + '/'
 
-a1 = Project('fdsi',0)
-print (a1.webpage)
+
+project_1 = Project(0, 'tiwa')
+project_2 = Project(1, 'fdsi')
+
+print('{} {} {}'.format(project_1.index, project_1.name, project_1.webpage))
+print('{} {} {}'.format(project_2.index, project_2.name, project_2.webpage))
